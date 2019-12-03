b025a5ace90e397c24417b8ba72ea98cc5497364
Angelus McNally <atmcnally@live.com>
2019-12-02 20:53:06 -0800

    almost done with all except remove all

--- bag_bst.h
+++ bag_bst.h
@@ -168,67 +168,50 @@ namespace coen79_lab9
         {   // Continue looking in the left subtree
             
             // STUDENT WORK
-            
+			bst_remove(root_ptr->left(), target);
         }
         
         if (target > root_ptr->data( ))
         {   // Continue looking in the right subtree
             
             // STUDENT WORK
+			bst_remove(root_ptr->right(), target);
         }
         
         // Target found
         if (root_ptr->left( ) == NULL)
-        {   // Target was found and there is no left subtree, so we can
-            // remove this node, making the right child be the new root.
+        {   // Target was found and there is no left subtree, so we can remove this node, making the right child be the new root.
 
             // STUDENT WORK
+			oldroot_ptr = root_ptr;
+			root_ptr = root_ptr->right();
+			delete oldroot_ptr;
+
         }
         
         // If code reaches this point, then we must remove the target from
-        // the current node. We'll actually replace this target with the
-        // maximum item of left subtree.
+        // the current node. We'll actually replace this target with the maximum item of left subtree.
 
         // STUDENT WORK
+
+		bst_remove_max(root_ptr->left(), root_ptr->data());
         
         return true;
     }
 
-	/* Case 4a. 
-	In this case of the bst_remove function, the root data is equal to the target, and the root node has no left child.
-
-oldroot_ptr = root_ptr;
-root_ptr = root_ptr->right( );
-delete oldroot_ptr;
-
-The variable oldroot_ptr is a local variable that we make point to the old root (the root that we are about to get rid of). 
-We then move the actual root pointer down to its right child. 
-And finally we execute, which returns the old root node to the heap.
-
-Case 4b. 
-In this case, the root node does have a left child, so we can’t simply
-move the root pointer to the right (as we did in case 4a). We could check
-whether there is a right child, and if not, we could certainly move the root
-pointer left—but we have a more general plan in mind. The plan is to find some
-entry in the non-empty left subtree, and move this entry up to the root. But which entry? Here’s an example to help you figure out which entry should be
-taken from the left subtree to replace the root entry.
-So, how do we delete the largest item from the left subtree, and
-place this same item at the root? We can use our own bst_remove_max function
-with the call:
-bst_remove_max(root_ptr->left( ), root_ptr->data( ));
-Now you know why we proposed the bst_remove_max function. Used in this
-way, it removes the largest item from the tree with root_ptr->left( ) as its
-root pointer, and it places this largest item in the root’s data field.*/
-    
+	/* The plan is to find some entry in the non-empty left subtree, and move this entry up to the root.
+	So, how do we delete the largest item from the left subtree, and place this same item at the root? 
+	We can use our own bst_remove_max function with the call:
+	bst_remove_max(root_ptr->left( ), root_ptr->data( ));
+	Now you know why we proposed the bst_remove_max function. Used in this way, 
+	it removes the largest item from the tree with root_ptr->left( ) as its root pointer, 
+	and it places this largest item in the root’s data field.*/
     
     template <class Item>
     typename bag<Item>::size_type bst_remove_all (binary_tree_node<Item>*& root_ptr, const Item& target)
-    // Precondition: root_ptr is a root pointer of a binary search tree
-    // or may be NULL for an empty tree).
-    // Postcondition: All copies of target have been removed from the tree
-    // and root_ptr now points to the root of the new
-    // (smaller) binary search tree. The return value tells how many copies
-    // of the target were removed.
+    // Precondition: root_ptr is a root pointer of a binary search tree (or may be NULL for an empty tree).
+    // Postcondition: All copies of target have been removed from the tree and root_ptr now points to the root of the new
+    // (smaller) binary search tree. The return value tells how many copies of the target were removed.
     {
         binary_tree_node<Item> *oldroot_ptr;
         
@@ -241,13 +224,14 @@ root pointer, and it places this largest item in the root’s data field.*/
         {   // Continue looking in the left subtree
             
             // STUDENT WORK
-            
+			bst_remove_all(root_ptr->left(), target);
         }
         
         if (target > root_ptr->data( ))
         {   // Continue looking in the right subtree
             
             // STUDENT WORK
+			bst_remove_all(root_ptr->right(), target);
         }
         
         if (root_ptr->left( ) == NULL)
@@ -255,17 +239,22 @@ root pointer, and it places this largest item in the root’s data field.*/
             // remove this node, making the right child be the new root.
 
             // STUDENT WORK
+			oldroot_ptr = root_ptr;
+			root_ptr = root_ptr->right();
+			delete oldroot_ptr;
+
         }
         
-        // If code reaches this point, then we must remove the target from
-        // the current node. We'll actually replace this target with the
-        // maximum item in our left subtree. We then continue
-        // searching for more copies of the target to remove.
-        // This continued search must start at the current root (since
-        // the maximum element that we moved up from our left subtree
-        // might also be a copy of the target).
+        // If code reaches this point, then we must remove the target from the current node. We'll actually replace this target with the
+        // maximum item in our left subtree. We then continue searching for more copies of the target to remove.
+        // This continued search must start at the current root (since the maximum element that we moved up from our left subtree might also be a copy of the target).
         
         // STUDENT WORK
+		//base case -- no right child, largest item at root
+
+		/* set removed equal to the data from the root, move the root pointer down to the left, and delete the root node. 
+		(2) On the other hand, if there is a right child, then there are larger items in the right subtree. 
+		In this case, make a recursive call to delete the largest item from the right subtree.*/
         
         return 1 + bst_remove_all(root_ptr, target);
     }
@@ -279,18 +268,16 @@ root pointer, and it places this largest item in the root’s data field.*/
     // ---------------------------------
 
     
-    // NOTE: The copy constructor needs to make a new copy of the source’s
-    // tree, and point root_ptr to the root of this copy
+    // NOTE: The copy constructor needs to make a new copy of the source’s tree, and point root_ptr to the root of this copy
     // Use the tree_copy function to do the copying
     template <class Item>
     bag<Item>::bag(const bag<Item>& source)
     // Library facilities used: bintree.h
     {
         // STUDENT WORK
+		root_ptr = tree_copy(source->root_ptr);
     }
     
-    
-
     template <class Item>
     bag<Item>::~bag( )
     // Header file used: bintree.h
@@ -305,10 +292,7 @@ root pointer, and it places this largest item in the root’s data field.*/
         return tree_size(root_ptr);
     }
     
-    
     // Insert
-    // Case 1: First handle this special case: When the first entry is inserted,
-    //         simply call root_ptr = new binary_tree_node<Item>(entry)
     // Case 2: There are already some other entries in the tree:
     //    • We pretend to search for the exact entry that we are trying to insert
     //    • We stop the search just before the cursor falls off the bottom of the tree,
@@ -332,17 +316,28 @@ root pointer, and it places this largest item in the root’s data field.*/
             {   // Go left
                 
                 // STUDENT WORK
-                
+				if (cursor->left() == NULL) {
+					cursor->set_left(entry);
+					done = true;
+				} else {
+					cursor = cursor->left();
+				}
+				
             }
             else
             {   // Go right
 
                 // STUDENT WORK
-                
+				if (cursor->right() == NULL) {
+					cursor->set_right(entry);
+					done = true;
+				}
+				else {
+					cursor = cursor->right();
+				}
             }
         }   while (!done);
     }
-    
     
     template <class Item>
     typename bag<Item>::size_type bag<Item>::count(const Item& target) const
@@ -358,11 +353,14 @@ root pointer, and it places this largest item in the root’s data field.*/
             else
             {
                 // STUDENT WORK
+				if (cursor->data() == target) {
+					answer++;
+				}
+				cursor = cursor->left();
             }
         }
         return answer;
     }
-    
     
     template <class Item>
     typename bag<Item>::size_type bag<Item>::erase(const Item& target)
@@ -376,19 +374,23 @@ root pointer, and it places this largest item in the root’s data field.*/
         return bst_remove(root_ptr, target);
     }
     
-    
     // The assignment operator:
     // 1. First check if it is a self-assignment by comparing (this == &source): If yes, then return
-    // 2. If there is no self-assignment, then before we copy the source tree we must release all
-    //    memory used by the nodes of the current tree
+    // 2. If there is no self-assignment, then before we copy the source tree we must release all memory used by the nodes of the current tree
     //    Use tree_clear to release memory
     template <class Item>
     void bag<Item>::operator =(const bag<Item>& source)
     // Header file used: bintree.h
     {
         // STUDENT WORK
+		if (this == &source) {
+			return;
+		}
+
+		tree_clear(source->root_ptr);
+		root_ptr = tree_copy(source->root_ptr());
+
     }
-    
     
     template <class Item>
     void bag<Item>::operator +=(const bag<Item>& addend)
@@ -410,14 +412,15 @@ root pointer, and it places this largest item in the root’s data field.*/
     bag<Item> operator +(const bag<Item>& b1, const bag<Item>& b2)
     {
         // STUDENT WORK
+		bag<Item> nbag = new bag(b1);
+		nbag += b2;
+		return nbag;
     }
-    
     
     template <class Item>
     void bag<Item>::insert_all(binary_tree_node<Item>* addroot_ptr)
     // Precondition: addroot_ptr is the root pointer of a binary search tree that
-    // is separate for the binary search tree of the bag that activated this
-    // method.
+    // is separate for the binary search tree of the bag that activated this method.
     // Postcondition: All the items from the addroot_ptr's binary search tree
     // have been added to the binary search tree of the bag that activated this
     // method.
@@ -428,6 +431,8 @@ root pointer, and it places this largest item in the root’s data field.*/
 			insert(addroot_ptr->data());
 			//Make a recursive call to insert all of addroot_ptr’s left subtree.
 			//Make a recursive call to insert all of addroot_ptr’s right subtree.
+			insert_all(addroot_ptr->left());
+			insert_all(addroot_ptr->right());
         }
     }
 
